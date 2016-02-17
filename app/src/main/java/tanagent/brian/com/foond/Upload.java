571e7aef584f9c811ef8d50d405ce4b742062f49
Brian Tan <briantanw@gmail.com>
2016-02-17 00:13:21 -0800

    able to select image from gallery and display it

--- app/src/main/java/tanagent/brian/com/foond/Upload.java
+++ app/src/main/java/tanagent/brian/com/foond/Upload.java
@@ -1,9 +1,13 @@
 package tanagent.brian.com.foond;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 /**
@@ -11,11 +15,25 @@ import android.widget.Toast;
  */
 public class Upload extends Activity{
 
-    private Button submitButton;
+    private static final int RESULT_LOAD_IMAGE = 1;
+    private ImageView foodImage;
+    private Button submitButton, selectImageButton;
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         this.setContentView(R.layout.upload);
+
+        foodImage = (ImageView) findViewById(R.id.selected_image);
+
+        selectImageButton = (Button) findViewById(R.id.gallery_button);
+        selectImageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent galleryIntent = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                startActivityForResult(galleryIntent, RESULT_LOAD_IMAGE);
+            }
+        });
+
 
         submitButton = (Button) findViewById(R.id.submit);
         submitButton.setOnClickListener(new View.OnClickListener() {
@@ -24,5 +42,14 @@ public class Upload extends Activity{
                 Toast.makeText(Upload.this, "The photo has been uploaded", Toast.LENGTH_LONG).show();
             }
         });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == RESULT_LOAD_IMAGE && resultCode == RESULT_OK && data != null) {
+            Uri selectedImage = data.getData();
+            foodImage.setImageURI(selectedImage);
+        }
     }
 }
--- app/src/main/java/tanagent/brian/com/foond/Yelp.java
+++ app/src/main/java/tanagent/brian/com/foond/Yelp.java
@@ -1,189 +1,189 @@
-//package tanagent.brian.com.foond;
-//
-//import android.app.Activity;
-//import android.os.Bundle;
-//import android.util.Log;
-//
-//import com.yelp.clientlib.connection.YelpAPI;
-//import com.yelp.clientlib.connection.YelpAPIFactory;
-//import com.yelp.clientlib.entities.SearchResponse;
-//import com.yelp.clientlib.entities.options.BoundingBoxOptions;
-//import com.yelp.clientlib.entities.options.CoordinateOptions;
-//
-//import junit.framework.Assert;
-//
-//import org.junit.Before;
-//import org.junit.Test;
-//
-//import java.io.IOException;
-//import java.util.HashMap;
-//import java.util.Map;
-//
-//import retrofit.Call;
-//import retrofit.Callback;
-//import retrofit.Response;
-//import retrofit.Retrofit;
-//
-///**
-// * Created by Brian on 2/10/2016.
-// */
-//public class Yelp extends Activity {
-//    private YelpAPI yelpAPI;
-//    private Response<SearchResponse> response;
-//    private Callback<SearchResponse> callback;
-//
-//    @Override
-//    protected void onCreate(Bundle savedInstanceState) {
-//        super.onCreate(savedInstanceState);
-//
-//        YelpAPIFactory yelpAPIFactory = new YelpAPIFactory(
-//                "uF70iaE55RtJ5aOCFPRXXQ",
-//                "pk3brkYNIwPSWdKP-YwRnTk7Y9M",
-//                "gLcpwL-ZNE3zIXGSJGoh4WI_ZfvgMb9Q",
-//                "peiZEgUYyCGkpVStBbLOg3PMCiM"
-//        );
-//
-//        // Make API requests to be executed in main thread so we can verify it easily.
-////        yelpAPIFactory = AsyncTestUtils.setToRunInMainThread(yelpAPIFactory);
-//
-//        yelpAPI = yelpAPIFactory.createAPI();
-//
-//        Map<String, String> params = new HashMap<>();
-//        // general params
-//        params.put("term", "food");
-//        params.put("limit", "3");
-//
-//        // locale params
-//        params.put("lang", "fr");
-//
-//        Call<SearchResponse> call = yelpAPI.search("San Francisco", params);
-//        try {
-//            response = call.execute();
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//            Log.i("TAG: ", "Something happened");
-//        }
-//
-//        call.enqueue(new Callback<SearchResponse>() {
+package tanagent.brian.com.foond;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.util.Log;
+
+import com.yelp.clientlib.connection.YelpAPI;
+import com.yelp.clientlib.connection.YelpAPIFactory;
+import com.yelp.clientlib.entities.SearchResponse;
+import com.yelp.clientlib.entities.options.BoundingBoxOptions;
+import com.yelp.clientlib.entities.options.CoordinateOptions;
+
+import junit.framework.Assert;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+import retrofit.Call;
+import retrofit.Callback;
+import retrofit.Response;
+import retrofit.Retrofit;
+
+/**
+ * Created by Brian on 2/10/2016.
+ */
+public class Yelp extends Activity {
+    private YelpAPI yelpAPI;
+    private Response<SearchResponse> response;
+    private Callback<SearchResponse> callback;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        YelpAPIFactory yelpAPIFactory = new YelpAPIFactory(
+                "uF70iaE55RtJ5aOCFPRXXQ",
+                "pk3brkYNIwPSWdKP-YwRnTk7Y9M",
+                "gLcpwL-ZNE3zIXGSJGoh4WI_ZfvgMb9Q",
+                "peiZEgUYyCGkpVStBbLOg3PMCiM"
+        );
+
+        // Make API requests to be executed in main thread so we can verify it easily.
+//        yelpAPIFactory = AsyncTestUtils.setToRunInMainThread(yelpAPIFactory);
+
+        yelpAPI = yelpAPIFactory.createAPI();
+
+        Map<String, String> params = new HashMap<>();
+        // general params
+        params.put("term", "food");
+        params.put("limit", "3");
+
+        // locale params
+        params.put("lang", "fr");
+
+        Call<SearchResponse> call = yelpAPI.search("San Francisco", params);
+        try {
+            response = call.execute();
+        } catch (IOException e) {
+            e.printStackTrace();
+            Log.i("TAG: ", "Something happened");
+        }
+
+        call.enqueue(new Callback<SearchResponse>() {
+            @Override
+            public void onResponse(Response<SearchResponse> response, Retrofit retrofit) {
+                SearchResponse searchResponse = response.body();
+//                Log.i("TAG: ", "Is this the response?" + searchResponse);
+            }
+
+            @Override
+            public void onFailure(Throwable t) {
+
+            }
+        });
+
+//        callback = new Callback<SearchResponse>() {
 //            @Override
 //            public void onResponse(Response<SearchResponse> response, Retrofit retrofit) {
-//                SearchResponse searchResponse = response.body();
-////                Log.i("TAG: ", "Is this the response?" + searchResponse);
-//            }
+//                Log.i("TAG", String.valueOf(response.body()));
 //
+//
+//                if (response.isSuccess())
+//                    Log.i("TAG", "I hope this works");
+//                else
+//                    Log.i("TAG", "I hope this works");
+//
+//                Log.i("TAG", "I hope this works");
+//
+//                SearchResponse searchResponse = response.body();
+//                // Update UI text with the searchResponse.
+//
+//                Log.i("TAG", "I hope this works");
+//            }
 //            @Override
 //            public void onFailure(Throwable t) {
-//
+//                // HTTP error happened, do something to handle it.
+//                Log.e("TAG", "It didn't work", t);
 //            }
-//        });
-//
-////        callback = new Callback<SearchResponse>() {
-////            @Override
-////            public void onResponse(Response<SearchResponse> response, Retrofit retrofit) {
-////                Log.i("TAG", String.valueOf(response.body()));
-////
-////
-////                if (response.isSuccess())
-////                    Log.i("TAG", "I hope this works");
-////                else
-////                    Log.i("TAG", "I hope this works");
-////
-////                Log.i("TAG", "I hope this works");
-////
-////                SearchResponse searchResponse = response.body();
-////                // Update UI text with the searchResponse.
-////
-////                Log.i("TAG", "I hope this works");
-////            }
-////            @Override
-////            public void onFailure(Throwable t) {
-////                // HTTP error happened, do something to handle it.
-////                Log.e("TAG", "It didn't work", t);
-////            }
-////        };
-//
-//
-//    }
-//
-//
-//    @Before
-//    public void setUp() {
-//        YelpAPIFactory yelpAPIFactory = new YelpAPIFactory(
-//                "uF70iaE55RtJ5aOCFPRXXQ",
-//                "pk3brkYNIwPSWdKP-YwRnTk7Y9M",
-//                "gLcpwL-ZNE3zIXGSJGoh4WI_ZfvgMb9Q",
-//                "peiZEgUYyCGkpVStBbLOg3PMCiM"
-//        );
-//
-//        // Make API requests to be executed in main thread so we can verify it easily.
-////        yelpAPIFactory = AsyncTestUtils.setToRunInMainThread(yelpAPIFactory);
-//
-//        yelpAPI = yelpAPIFactory.createAPI();
-//    }
-//
-//
-//    @Test
-//    public void testSearchByLocation() throws IOException {
-//        Map<String, String> params = new HashMap<>();
-//        params.put("term", "yelp");
-//
-//        Call<SearchResponse> call = yelpAPI.search("San Francisco", params);
-//        Response<SearchResponse> response = call.execute();
-//        Assert.assertEquals(200, response.code());
-//
-//        SearchResponse searchResponse = response.body();
-//        Assert.assertNotNull(searchResponse);
-//    }
-//
-//    @Test
-//    public void testSearchByLocationWithOptionalCoordinate() throws IOException {
-//        Map<String, String> params = new HashMap<>();
-//        params.put("term", "yelp");
-//        params.put("cll", "37.7867703362929,-122.399958372115");
-//
-//        Call<SearchResponse> call = yelpAPI.search("San Francisco", params);
-//        Response<SearchResponse> response = call.execute();
-//        Assert.assertEquals(200, response.code());
-//
-//        SearchResponse searchResponse = response.body();
-//        Assert.assertNotNull(searchResponse);
-//    }
-//
-//
-//    @Test
-//    public void testSearchByCoordinateOptions() throws IOException {
-//        CoordinateOptions coordinate = CoordinateOptions.builder()
-//                .latitude(37.7867703362929)
-//                .longitude(-122.399958372115).build();
-//
-//        Map<String, String> params = new HashMap<>();
-//        params.put("term", "yelp");
-//
-//        Call<SearchResponse> call = yelpAPI.search(coordinate, params);
-//        Response<SearchResponse> response = call.execute();
-//        Assert.assertEquals(200, response.code());
-//
-//        SearchResponse searchResponse = response.body();
-//        Assert.assertNotNull(searchResponse);
-//    }
-//
-//    @Test
-//    public void testSearchByBoundingBoxOptions() throws IOException {
-//        BoundingBoxOptions bounds = BoundingBoxOptions.builder()
-//                .swLatitude(37.900000)
-//                .swLongitude(-122.500000)
-//                .neLatitude(37.788022)
-//                .neLongitude(-122.399797)
-//                .build();
-//
-//        Map<String, String> params = new HashMap<>();
-//        params.put("term", "yelp");
-//
-//        Call<SearchResponse> call = yelpAPI.search(bounds, params);
-//        Response<SearchResponse> response = call.execute();
-//        Assert.assertEquals(200, response.code());
-//
-//        SearchResponse searchResponse = response.body();
-//        Assert.assertNotNull(searchResponse);
-//    }
-//}
+//        };
+
+
+    }
+
+
+    @Before
+    public void setUp() {
+        YelpAPIFactory yelpAPIFactory = new YelpAPIFactory(
+                "uF70iaE55RtJ5aOCFPRXXQ",
+                "pk3brkYNIwPSWdKP-YwRnTk7Y9M",
+                "gLcpwL-ZNE3zIXGSJGoh4WI_ZfvgMb9Q",
+                "peiZEgUYyCGkpVStBbLOg3PMCiM"
+        );
+
+        // Make API requests to be executed in main thread so we can verify it easily.
+//        yelpAPIFactory = AsyncTestUtils.setToRunInMainThread(yelpAPIFactory);
+
+        yelpAPI = yelpAPIFactory.createAPI();
+    }
+
+
+    @Test
+    public void testSearchByLocation() throws IOException {
+        Map<String, String> params = new HashMap<>();
+        params.put("term", "yelp");
+
+        Call<SearchResponse> call = yelpAPI.search("San Francisco", params);
+        Response<SearchResponse> response = call.execute();
+        Assert.assertEquals(200, response.code());
+
+        SearchResponse searchResponse = response.body();
+        Assert.assertNotNull(searchResponse);
+    }
+
+    @Test
+    public void testSearchByLocationWithOptionalCoordinate() throws IOException {
+        Map<String, String> params = new HashMap<>();
+        params.put("term", "yelp");
+        params.put("cll", "37.7867703362929,-122.399958372115");
+
+        Call<SearchResponse> call = yelpAPI.search("San Francisco", params);
+        Response<SearchResponse> response = call.execute();
+        Assert.assertEquals(200, response.code());
+
+        SearchResponse searchResponse = response.body();
+        Assert.assertNotNull(searchResponse);
+    }
+
+
+    @Test
+    public void testSearchByCoordinateOptions() throws IOException {
+        CoordinateOptions coordinate = CoordinateOptions.builder()
+                .latitude(37.7867703362929)
+                .longitude(-122.399958372115).build();
+
+        Map<String, String> params = new HashMap<>();
+        params.put("term", "yelp");
+
+        Call<SearchResponse> call = yelpAPI.search(coordinate, params);
+        Response<SearchResponse> response = call.execute();
+        Assert.assertEquals(200, response.code());
+
+        SearchResponse searchResponse = response.body();
+        Assert.assertNotNull(searchResponse);
+    }
+
+    @Test
+    public void testSearchByBoundingBoxOptions() throws IOException {
+        BoundingBoxOptions bounds = BoundingBoxOptions.builder()
+                .swLatitude(37.900000)
+                .swLongitude(-122.500000)
+                .neLatitude(37.788022)
+                .neLongitude(-122.399797)
+                .build();
+
+        Map<String, String> params = new HashMap<>();
+        params.put("term", "yelp");
+
+        Call<SearchResponse> call = yelpAPI.search(bounds, params);
+        Response<SearchResponse> response = call.execute();
+        Assert.assertEquals(200, response.code());
+
+        SearchResponse searchResponse = response.body();
+        Assert.assertNotNull(searchResponse);
+    }
+}
